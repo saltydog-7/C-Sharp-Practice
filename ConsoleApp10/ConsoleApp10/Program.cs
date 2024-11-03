ac32f6144f70623d12d6d481e6c73bdaa71a6302
saltydog-7 <rvn.kwong@outlook.jp>
2024-11-03 19:05:31 +0900

    Add files via upload

---
+++ ConsoleApp10/ConsoleApp10/Program.cs
@@ -0,0 +1,30 @@
+﻿// for 迴圈
+
+/* 將以下while迴圈以for迴圈表示
+ * int i = 1;
+ * while(i<=5){
+ * System.Console.Write(i);
+ * i++
+ * }
+*/
+
+
+//for()內有3個部分
+//第一個部分設定變數初始值 即 int i = 1
+//第二個部分設定條件 即 i<=5
+//第三個部分設定每次執行完迴圈後要處理的事
+
+for(int i=1; i<=5; i++)
+{
+    System.Console.WriteLine(i);
+}
+
+//Array應用
+int[] nums = { 5, 10, 17, 21, 26, 28 }; //設置陣列
+for(int a=0; a<nums.Length; a++)
+    //a初始值為0 即 陣列第一個數字
+    //a小於nums陣列內有多少組數字（目前nums內有6組數字）
+    //每次執行迴圈後a會+1
+{
+    System.Console.WriteLine(nums[a]); //輸出陣列
+}
---
+++ ConsoleApp12/ConsoleApp12/Program.cs
@@ -0,0 +1,18 @@
+﻿// class 類別
+// object 物件
+
+//Person為資料型態 - class
+//由class所創建的東西person_1為物件
+
+Person person_1 = new Person();
+person_1.height = 170.5; //代入170.5到person_1的height
+person_1.age = 42;
+person_1.name = "Tom";
+System.Console.WriteLine(person_1.height);
+
+//Create new object
+Person person_2 = new Person();
+person_2.height = 162.3;
+person_2.age = 37;
+person_2.name = "Mary";
+System.Console.WriteLine(person_2.height);
